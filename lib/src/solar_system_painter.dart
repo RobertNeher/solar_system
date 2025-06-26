c07558f097cb8662cf1ed71dd4859c85489db24a
RobertNeher <Robert.Neher@gmail.com>
2025-06-26 18:27:13 +0200

    Customizable sun, orbits parameter via settings

--- lib/src/solar_system_painter.dart
+++ lib/src/solar_system_painter.dart
@@ -2,6 +2,7 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:solar_system/src/helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SolarSystemPainter extends CustomPainter {
   final double animationValue;
@@ -19,9 +20,32 @@ class SolarSystemPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final Offset center = Offset(size.width / 2, size.height / 2);
 
-    // Zeichne die Sonne.
-    final sunPaint = Paint()..color = colorFromString(settings['sunColor']);
-    canvas.drawCircle(center, settings['sunSize'], sunPaint);
+    // Central star
+    final TextStyle sunNameStyle = TextStyle(
+      fontFamily: settings['sun']['font'],
+      color: colorFromString(settings['sun']['fontColor']),
+      fontSize: settings['sun']['fontSize'].toDouble(),
+    );
+    final sunPaint = Paint()..color = colorFromString(settings['sun']['color']);
+    canvas.drawCircle(center, settings['sun']['radius'].toDouble(), sunPaint);
+
+    TextSpan sunName = TextSpan(
+      text: settings['sun']['name'],
+      style: sunNameStyle,
+    );
+    TextPainter namePainter = TextPainter(
+      text: sunName,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    namePainter.layout();
+    namePainter.paint(
+      canvas,
+      Offset(
+        center.dx - namePainter.width / 2,
+        center.dy - namePainter.height / 2,
+      ),
+    );
 
     // Draw each planet on the list
     for (var planet in planets) {
@@ -29,15 +53,16 @@ class SolarSystemPainter extends CustomPainter {
       final Color planetColor = colorFromString(planet['color']);
       final double planetRadius = planet['radius'].toDouble();
       final double speed = planet['speed'].toDouble();
-      // Draw orbits
-      final orbitPaint = Paint()
-        ..color = Colors.white12
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = 0.5;
-      canvas.drawCircle(center, orbitalRadius, orbitPaint);
 
+      if (settings['orbits']['visible']) {
+        // Draw orbits
+        final orbitPaint = Paint()
+          ..color = colorFromString(settings['orbits']['color'])
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = settings['orbits']['width'].toDouble();
+        canvas.drawCircle(center, orbitalRadius, orbitPaint);
+      }
       final double angle = (orbitalValue * speed * 2 * math.pi) % (2 * math.pi);
-
       final double planetX = center.dx + orbitalRadius * math.cos(angle);
       final double planetY = center.dy + orbitalRadius * math.sin(angle);
       final Offset planetPosition = Offset(planetX, planetY);
@@ -45,7 +70,7 @@ class SolarSystemPainter extends CustomPainter {
       final TextStyle planetNameStyle = TextStyle(
         fontFamily: settings['planetName']['font'],
         color: colorFromString(settings['planetName']['fontColor']),
-        fontSize: settings['planetName']['fontSize'],
+        fontSize: settings['planetName']['fontSize'].toDouble(),
       );
       canvas.drawCircle(planetPosition, planetRadius, planetPaint);
       TextSpan planetName = TextSpan(
@@ -58,7 +83,13 @@ class SolarSystemPainter extends CustomPainter {
         textDirection: TextDirection.ltr,
       );
       namePainter.layout();
-      namePainter.paint(canvas, planetPosition);
+      namePainter.paint(
+        canvas,
+        Offset(
+          planetPosition.dx - namePainter.width / 2,
+          planetPosition.dy - namePainter.height / 2,
+        ),
+      );
     }
   }
 
