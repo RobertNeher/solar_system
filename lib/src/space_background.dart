7ec81b6e96dff1a2b1276620b57e8c658bcb5049
rneher <robert.neher@bartscher-hasenaecker.de>
2025-06-26 18:39:10 +0200

    stellar background added (from predecessor project)

---
+++ lib/src/space_background.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:solar_system/src/space_background_data.dart';
+import 'package:solar_system/src/star_painter.dart';
+
+List<Widget> spaceBackground(
+  double windowSize,
+  SpaceBackgroundData backgroundData,
+) {
+  List<Widget> starsInSpace = <Widget>[];
+
+  starsInSpace.add(
+    Container(
+      width: windowSize,
+      height: windowSize,
+      color: backgroundData.backgroundColor,
+    ),
+  );
+
+  if (!backgroundData.stars) {
+    return starsInSpace;
+  }
+
+  CustomPaint star = CustomPaint(
+    painter: StarPainter(
+      starSize: backgroundData.starSize,
+      starColor: backgroundData.starColor,
+      edges: backgroundData.starEdges,
+    ),
+  );
+
+  for (int i = 0; i < backgroundData.maxStars; i++) {
+    Positioned positionedStar = Positioned(
+      left: Random().nextDouble() * windowSize,
+      top: Random().nextDouble() * windowSize,
+      child: star,
+    );
+    starsInSpace.add(positionedStar);
+  }
+  return starsInSpace;
+}
